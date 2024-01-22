f7a8aee45870d2d26962783db5e19841fa93d946
hadi0o <hadi.deh19@gmail.com>
2024-01-22 13:34:25 +0330

    accordion panel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccordionModule } from "primeng/accordion";
+import {AvatarModule} from "primeng/avatar";
+import {BadgeModule} from "primeng/badge";
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AccordionModule,
+    AppRoutingModule,
+    AvatarModule,
+    BadgeModule
   ],
   providers: [
     provideClientHydration()
